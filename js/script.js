af9e591f0cf7aa0080a74349a807f9546e220598
abc <liukaijv@foxmail.com>
2021-12-19 22:31:56 +0800

    Site updated: 2021-12-19 22:31:55

---
+++ js/script.js
@@ -0,0 +1,11 @@
+(function () {
+
+    // toggle nav
+    $header = document.getElementById('header');
+    document.getElementById('toggle-nav').addEventListener('click', function () {
+        $header.classList.toggle('menu-opened');
+    });
+
+})();
+
+
