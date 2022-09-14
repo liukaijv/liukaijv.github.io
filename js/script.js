93f52620d42c7b65576013d2ce5b6e0dc4a70bb3
abc <liukaijv@foxmail.com>
2022-09-14 21:58:25 +0800

    Site updated: 2022-09-14 21:58:23

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 (function () {
 
     // toggle nav
-    $header = document.getElementById('header');
+    var $header = document.getElementById('header');
     document.getElementById('toggle-nav').addEventListener('click', function () {
         $header.classList.toggle('menu-opened');
     });
